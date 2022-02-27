170d3de33cb3680197979b3c633c97b96b49c49d
Lee Jackson <lee.jackson@tomorrowland.fortyfivetwo.net>
2022-02-27 19:27:53 +0000

    #2 - feat: add uuid gen lib to the project and test it on the main page;

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <template>
   <main>
     <h1>Welcome to Vienna App</h1>
+    <p>Current page id: <strong>{{ pageLoadId }}</strong></p>
+    <button class="btn btn-primary" @click="generateNewId">Generate new page id</button>
   </main>
 </template>
+
+<script setup lang="ts">
+import { v4 as uuidv4 } from "uuid";
+import { ref } from 'vue'
+
+const  pageLoadId = ref(uuidv4())
+
+function generateNewId() {
+  pageLoadId.value = uuidv4()
+}
+
+</script>
